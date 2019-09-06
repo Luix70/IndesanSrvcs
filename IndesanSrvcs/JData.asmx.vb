838bcda7ea01ca012a098a862d508d61252b68cf
Luix70 <lortuno1@gmail.com>
2019-09-06 17:59:38 +0200

    unificacion de JData y JWT

---
+++ IndesanSrvcs/JData.asmx.vb
@@ -0,0 +1,330 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Services
+Imports System.Web.Script.Services
+Imports System.ComponentModel
+Imports System.Web.Script.Serialization
+Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Web.Services.Protocols
+' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
+
+<System.Web.Services.WebService(Namespace:="http://servicios.indesan.com/")>
+<System.Web.Script.Services.ScriptService>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
+Public Class JData
+	Inherits System.Web.Services.WebService
+
+	Private Const StrResponseNew As String = "Hello World"
+	Public SoapHeader As TokenAuthUser
+
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	Public Sub JPedidos()
+
+		Dim parCodRep As String = HttpContext.Current.Request.Params("cr")
+		Dim parCodCli As String = HttpContext.Current.Request.Params("cc")
+		Dim parCodigodoc As String = HttpContext.Current.Request.Params("cd")
+		Dim parTipodoc As String = HttpContext.Current.Request.Params("td")
+
+		Dim qj As New QueryJson()
+
+		Dim resultados As String
+
+		resultados = qj.GenerarJson(parCodRep, parCodCli, parTipodoc, parCodigodoc)
+
+		HttpContext.Current.Response.Clear()
+		HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*")
+		HttpContext.Current.Response.ContentType = "application/json;charset=UTF-8"
+		HttpContext.Current.Response.Write(resultados)
+
+
+	End Sub
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	Public Sub JOps()
+
+		Dim parUser As String = HttpContext.Current.Request.Params("user")
+		Dim parPassWord As String = HttpContext.Current.Request.Params("password")
+
+		Dim qj As New QueryJson()
+
+		Dim resultados As String
+
+		resultados = qj.GenerarJson2(parUser, parPassWord)
+
+		HttpContext.Current.Response.Clear()
+		HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*")
+		' HttpContext.Current.Response.AppendHeader("Content-Encoding", "identity")
+		HttpContext.Current.Response.ContentType = "application/json;charset=UTF-8"
+		HttpContext.Current.Response.Write(resultados)
+
+
+	End Sub
+
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	Public Sub JSaluda()
+
+		Dim js As New JavaScriptSerializer
+		Dim saludo As New atomObject(StrResponseNew)
+		HttpContext.Current.Response.Clear()
+		HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*")
+		' HttpContext.Current.Response.AppendHeader("Content-Encoding", "identity")
+		HttpContext.Current.Response.ContentType = "application/json;charset=UTF-8"
+		HttpContext.Current.Response.Write(js.Serialize(saludo))
+
+
+
+	End Sub
+	'<SoapHeader("user")>
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	Public Sub JAuth()
+
+		Dim parUser As String = HttpContext.Current.Request.Params("user")
+		Dim parPassWord As String = HttpContext.Current.Request.Params("password")
+		Dim strRespuesta As String = "Usuario inválido"
+		Dim usr As New AuthUser
+		usr.Username = parUser
+		usr.Password = parPassWord
+		Dim NombreUsuario As String = ""
+
+		If usr.isValid(NombreUsuario) Then
+
+			strRespuesta = "Bienvenido, " + NombreUsuario
+
+		End If
+
+		Dim js As New JavaScriptSerializer
+		Dim saludo As New atomObject(strRespuesta)
+		HttpContext.Current.Response.Clear()
+		HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*")
+		' HttpContext.Current.Response.AppendHeader("Content-Encoding", "identity")
+		HttpContext.Current.Response.ContentType = "application/json;charset=UTF-8"
+		HttpContext.Current.Response.Write(js.Serialize(saludo))
+
+	End Sub
+
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	<SoapHeader("SoapHeader")>
+	Public Function JTokenAuth() As String
+
+		If SoapHeader Is Nothing Then
+			Return "Necesitas autentificarte antes de acceder a este método. Llama a AuthenticationMethod()"
+		End If
+
+		If Not SoapHeader.isUserCredentialsValid(SoapHeader) Then
+			Return "Necesitas autentificarte antes de acceder a este método. Llama a AuthenticationMethod()"
+		Else
+			Return "Hola, " + HttpRuntime.Cache(SoapHeader.AuthenticationToken)
+		End If
+	End Function
+
+	<WebMethod()>
+	<ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+	<SoapHeader("SoapHeader")>
+	Public Function AuthenticationMethod() As String
+		If SoapHeader Is Nothing Then
+			Return "Por favor, suministra un SoapHeader"
+		End If
+
+		If String.IsNullOrEmpty(SoapHeader.Username) OrElse String.IsNullOrEmpty(SoapHeader.Password) Then
+			Return "Por favor, suministra nombre de ususario y contraseña"
+		End If
+
+		If Not SoapHeader.isUserCredentialsValid(SoapHeader.Username, SoapHeader.Password) Then
+
+			Return "Usuario o contraseña inválidos"
+
+		End If
+
+		Dim token As String = Guid.NewGuid().ToString()
+
+		HttpRuntime.Cache.Add(token,
+							  SoapHeader.Username,
+							  Nothing,
+							  System.Web.Caching.Cache.NoAbsoluteExpiration,
+							  TimeSpan.FromMinutes(30),
+							  System.Web.Caching.CacheItemPriority.NotRemovable,
+							  Nothing)
+		Return token
+
+	End Function
+	Private Class atomObject
+		Private strResponse As String
+		Public Sub New(ByVal strResponseNew As String)
+			Me.strResponse = strResponseNew
+		End Sub
+		Public Property response As String
+			Get
+				Return strResponse
+			End Get
+
+
+			Set(value As String)
+				strResponse = value
+			End Set
+		End Property
+	End Class
+
+End Class
+
+Public Class AuthUser
+	Inherits System.Web.Services.Protocols.SoapHeader
+	Private strUsername As String
+	Public Property Username() As String
+		Get
+			Return strUsername
+		End Get
+		Set(ByVal value As String)
+			strUsername = value
+		End Set
+	End Property
+
+	Private newPassword As String
+	Public Property Password() As String
+		Get
+			Return newPassword
+		End Get
+		Set(ByVal value As String)
+			newPassword = value
+		End Set
+	End Property
+
+	Public Function isValid(ByRef strNombreCliente As String) As Boolean
+		'Realizar toda la lógica para determinar si el usuario es válido
+		'(Consultar database etc)
+		Dim count As Int16 = 0
+		Dim config As String = ConfigurationManager.ConnectionStrings("myCon").ConnectionString
+
+		Dim strCadenaConsulta As String
+
+		strCadenaConsulta = "Select * from CLIENTES_RST where nombreusuario ='" + Username + "' AND contras ='" + Password + "';"
+		Dim Cons As New OleDb.OleDbConnection
+
+		Cons.ConnectionString = config
+
+		Cons.Open()
+
+		Dim ndt As New DataTable
+		Dim dad As New OleDb.OleDbDataAdapter(strCadenaConsulta, Cons)
+
+		Try
+			dad.Fill(ndt)
+		Catch ex As Exception
+			MsgBox(ex.Message)
+		End Try
+
+		count = ndt.Rows.Count
+
+		Dim result As Boolean
+
+		If count > 0 Then
+
+			result = True
+			strNombreCliente = ndt.Rows(0).Item("rzs")
+		Else
+			result = False
+			strNombreCliente = ""
+		End If
+
+
+		Cons.Close()
+		Cons = Nothing
+		Return result
+
+	End Function
+End Class
+
+Public Class TokenAuthUser
+	Inherits System.Web.Services.Protocols.SoapHeader
+	Private strUsername As String
+	Public Property Username() As String
+		Get
+			Return strUsername
+		End Get
+		Set(ByVal value As String)
+			strUsername = value
+		End Set
+	End Property
+
+	Private newPassword As String
+	Public Property Password() As String
+		Get
+			Return newPassword
+		End Get
+		Set(ByVal value As String)
+			newPassword = value
+		End Set
+	End Property
+	Private newAuthenticationToken As String
+	Public Property AuthenticationToken() As String
+		Get
+			Return newAuthenticationToken
+		End Get
+		Set(ByVal value As String)
+			newAuthenticationToken = value
+		End Set
+	End Property
+	Public Function isUserCredentialsValid(username As String, password As String) As Boolean
+		'Realizar toda la lógica para determinar si el usuario es válido
+		'(Consultar database etc)
+		Dim count As Int16 = 0
+		Dim config As String = ConfigurationManager.ConnectionStrings("myCon").ConnectionString
+
+		Dim strCadenaConsulta As String
+
+		strCadenaConsulta = "Select * from CLIENTES_RST where nombreusuario ='" + username + "' AND contras ='" + password + "';"
+		Dim Cons As New OleDb.OleDbConnection
+
+		Cons.ConnectionString = config
+
+		Cons.Open()
+
+		Dim ndt As New DataTable
+		Dim dad As New OleDb.OleDbDataAdapter(strCadenaConsulta, Cons)
+
+		Try
+			dad.Fill(ndt)
+		Catch ex As Exception
+			MsgBox(ex.Message)
+		End Try
+
+		count = ndt.Rows.Count
+
+		Dim result As Boolean
+
+		If count > 0 Then
+
+			result = True
+
+		Else
+			result = False
+
+		End If
+
+
+		Cons.Close()
+		Cons = Nothing
+		Return result
+
+	End Function
+
+	Public Function isUserCredentialsValid(SoapHeader As TokenAuthUser) As Boolean
+		If SoapHeader Is Nothing Then
+			Return False
+		End If
+
+		If Not String.IsNullOrEmpty(SoapHeader.AuthenticationToken) Then
+			Return HttpRuntime.Cache(SoapHeader.AuthenticationToken) IsNot Nothing
+		End If
+
+		Return False
+
+	End Function
+End Class
---
+++ IndesanSrvcs/QueryJson.vb
@@ -0,0 +1,463 @@
+﻿Imports System.Web.Script.Serialization
+Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Configuration
+Public Class QueryJson
+
+	Shared strConexion As String = ConfigurationManager.ConnectionStrings("myCon").ConnectionString
+	Shared dt As DataTable
+	Shared intCurrentRow As Integer = 0
+	Private Class Resultado
+		Public consulta As String
+		Public FechaConsulta As String
+		Public representantes As New Collection
+		Public totalRepresentantes As Integer
+		Public Status As String
+		Public errorcode As String
+
+		Function ObtenerRepresentantes() As Integer
+
+			'Poblar la coleccion de representantes y dar valor al totalRepresentantes
+			Dim reps As Integer = 0
+			Dim objRep As Representante
+
+			Dim intRows As Integer = dt.Rows.Count
+
+
+			While intRows > 0 And intCurrentRow < intRows
+
+				reps += 1
+
+				Dim codrep As Integer = dt.Rows(intCurrentRow).Item("codrep")
+				Dim NombreRepresentante As String = dt.Rows(intCurrentRow).Item("nombre")
+				objRep = New Representante
+				objRep.codrep = codrep
+				objRep.nombre = NombreRepresentante
+
+				' El método obtenerClientes debe depositar el cursor del DbReader en el ultimo registro
+				' correspondiente al representante actual
+				objRep.totalClientes = objRep.obtenerClientes(codrep)
+
+				representantes.Add(objRep, codrep.ToString)
+
+			End While
+
+			Return reps
+
+		End Function
+	End Class
+
+	Private Class Representante
+		Public codrep As Long
+		Public nombre As String
+		Public clientes As New Collection
+		Public totalClientes As Integer
+
+		Function obtenerClientes(codRepActual As Integer) As Integer
+
+			Dim n_clientes As Integer = 0
+			Dim objCliente As Cliente
+
+			While intCurrentRow < dt.Rows.Count AndAlso dt.Rows(intCurrentRow).Item("codrep") = codRepActual
+
+				n_clientes += 1
+				objCliente = New Cliente
+
+				objCliente.codigo = dt.Rows(intCurrentRow).Item("codigo")
+				objCliente.rzs = dt.Rows(intCurrentRow).Item("rzs")
+				objCliente.poblacion = dt.Rows(intCurrentRow).Item("poblacion")
+
+				objCliente.totalDocumentos = objCliente.obtenerDocumentos(objCliente.codigo)
+
+				clientes.Add(objCliente, objCliente.codigo.ToString)
+
+
+			End While
+
+			' devolvemos el cursor al ultimo registro del cliente o al ultimo de la tabla
+
+			Return n_clientes
+
+		End Function
+
+	End Class
+
+	Private Class Cliente
+		Public codigo As String
+		Public rzs As String
+		Public poblacion As String
+		Public documentos As New Collection
+		Public totalDocumentos As Integer
+
+		Function obtenerDocumentos(codigo As String) As Integer
+
+			Dim n_documentos As Integer = 0
+			Dim objDoc As Documento
+
+			While intCurrentRow < dt.Rows.Count AndAlso dt.Rows(intCurrentRow).Item("codigo") = codigo
+
+				objDoc = New Documento
+				n_documentos += 1
+
+				objDoc.tipodoc = dt.Rows(intCurrentRow).Item("tipodoc")
+				objDoc.codigodoc = dt.Rows(intCurrentRow).Item("codigodoc")
+				objDoc.fechapedido = dt.Rows(intCurrentRow).Item("fecha1").ToString
+				objDoc.Importebruto = dt.Rows(intCurrentRow).Item("Bruto")
+
+				If IsDBNull(dt.Rows(intCurrentRow).Item("referencia")) Then
+					objDoc.referencia = ""
+				Else
+					objDoc.referencia = dt.Rows(intCurrentRow).Item("referencia")
+				End If
+
+				objDoc.totalLineas = objDoc.obtenerLineas(objDoc.tipodoc, objDoc.codigodoc)
+
+				documentos.Add(objDoc, objDoc.tipodoc & objDoc.codigodoc.ToString)
+
+
+
+			End While
+
+			Return n_documentos
+
+		End Function
+
+	End Class
+
+	Private Class Documento
+		Public tipodoc As String
+		Public codigodoc As Long
+		Public fechapedido As String
+		Public Importebruto As Single
+		Public referencia As String
+		Public lineas As New Collection
+		Public totalLineas As Integer
+
+		Function obtenerLineas(tipodoc As String, codigodoc As Long) As Integer
+			Dim n_lineas As Integer = 0
+			Dim objLinea As Linea
+
+			While intCurrentRow < dt.Rows.Count AndAlso dt.Rows(intCurrentRow).Item("tipodoc") = tipodoc AndAlso dt.Rows(intCurrentRow).Item("codigodoc") = codigodoc
+				n_lineas += 1
+				objLinea = New Linea
+
+				objLinea.linea = dt.Rows(intCurrentRow).Item("linea")
+				objLinea.coart = dt.Rows(intCurrentRow).Item("coart")
+				Try
+					If IsDBNull(dt.Rows(intCurrentRow).Item("cantidad")) Then
+						objLinea.cantidad = 0
+					Else
+						objLinea.cantidad = dt.Rows(intCurrentRow).Item("cantidad")
+					End If
+				Catch e As Exception
+					objLinea.cantidad = 0
+				End Try
+
+
+				objLinea.descripcion = dt.Rows(intCurrentRow).Item("descripcion")
+				If IsDBNull(dt.Rows(intCurrentRow).Item("ref_linea")) Then
+					objLinea.ref_linea = ""
+				Else
+					objLinea.ref_linea = dt.Rows(intCurrentRow).Item("ref_linea")
+				End If
+
+				objLinea.precio = dt.Rows(intCurrentRow).Item("precio")
+
+				lineas.Add(objLinea, n_lineas.ToString)
+
+				intCurrentRow += 1
+
+			End While
+
+			Return n_lineas
+
+
+
+		End Function
+
+	End Class
+	Private Class Linea
+		Public linea As Integer
+		Public coart As String
+		Public cantidad As Single
+		Public descripcion As String
+		Public ref_linea As String
+		Public precio As Single
+	End Class
+
+	Public Function GenerarJson(parCodRep As String, parCodCli As String, parTipodoc As String, parCodigodoc As String) As String
+
+
+		Dim Json As String = ""
+		dt = New DataTable
+		intCurrentRow = 0
+		Dim js As JavaScriptSerializer
+		js = New JavaScriptSerializer()
+
+		Dim strCadenaConsulta As String
+		strCadenaConsulta = CadenaConsulta(parCodRep, parCodCli, parTipodoc, parCodigodoc)
+
+		Dim Cons As New OleDb.OleDbConnection
+		Cons.ConnectionString = strConexion
+		Cons.Open()
+
+		Using dad As New OleDb.OleDbDataAdapter(strCadenaConsulta, Cons)
+
+			dad.Fill(dt)
+
+		End Using
+
+		Cons.Close()
+		Cons = Nothing
+
+		Dim res As New Resultado
+		res.consulta = strCadenaConsulta
+		res.FechaConsulta = Now.ToShortDateString & " " & Now.ToShortTimeString
+		res.totalRepresentantes = res.ObtenerRepresentantes()
+
+		Json = js.Serialize(res)
+
+		dt = Nothing
+
+
+		Return Json
+
+	End Function
+
+	Public Function GenerarJson2(parUsuario As String, parPassword As String) As String
+
+		Dim Json As String = ""
+
+		intCurrentRow = 0
+		Dim js As JavaScriptSerializer
+		js = New JavaScriptSerializer()
+		js.MaxJsonLength = 10000000
+
+		Dim strCadenaConsulta As String
+
+		strCadenaConsulta = "SELECT * FROM Credenciales_rst WHERE Credenciales_rst.NombreUsuario ='" & parUsuario & "';"
+		Dim Cons As New OleDb.OleDbConnection
+		Cons.ConnectionString = strConexion
+		Cons.Open()
+
+		dt = New DataTable
+
+		Using dad As New OleDb.OleDbDataAdapter(strCadenaConsulta, Cons)
+
+			Try
+				dad.Fill(dt)
+			Catch ex As Exception
+				MsgBox(ex.Message)
+			End Try
+
+
+		End Using
+
+		Cons.Close()
+		Cons = Nothing
+
+		Dim res As New Resultado
+
+		If dt.Rows.Count = 0 Then
+			res.consulta = "nulo"
+			res.Status = "noOK"
+			res.errorcode = "Usuario Inexistente"
+			res.FechaConsulta = Now.ToShortDateString & " " & Now.ToShortTimeString
+			Json = js.Serialize(res)
+			Return Json
+
+		Else
+			If dt.Rows(0).Field(Of String)("Contraseña") <> parPassword Then
+				res.consulta = "nulo"
+				res.Status = "noOK"
+				res.errorcode = "Contraseña errónea"
+				res.FechaConsulta = Now.ToShortDateString & " " & Now.ToShortTimeString
+				Json = js.Serialize(res)
+				Return Json
+			Else
+				'Todo correcto
+				res.consulta = CadenaConsulta2("{tipoentidad:'" & dt.Rows(0).Field(Of String)("TipoEntidad") & "', AccesoCli:'" & dt.Rows(0).Field(Of String)("AccesoCli") & "', AccesoRep: '" & dt.Rows(0).Field(Of String)("AccesoRep") & "'}")
+				res.Status = "OK"
+				res.errorcode = ""
+				'primero vamos a ver si existe una copia en el cache reciente, para evitar llamadas a la base de datos
+				If File.Exists(HttpContext.Current.Server.MapPath(".") & "\JSON\result.json") And (res.consulta = "SELECT * FROM listadoOperaciones where (codrep > 0);") Then
+
+					Dim strJson As String
+					Using sr As New StreamReader(HttpContext.Current.Server.MapPath(".") & "\JSON\result.json")
+
+						' Read the stream to a string and write the string to the console.
+						strJson = sr.ReadToEnd()
+
+					End Using
+
+					res = js.Deserialize(Of Resultado)(strJson)
+
+					If DateDiff(DateInterval.Minute, Convert.ToDateTime(res.FechaConsulta), Date.Now()) < 60 Then
+						Return strJson
+					Else ' file is old
+
+						Json = ConsultarDB(res.consulta)
+						Return Json
+
+					End If
+				Else 'file doesn't exists
+
+					Json = ConsultarDB(res.consulta)
+					Return Json
+
+				End If
+			End If
+		End If
+
+	End Function
+	Function ConsultarDB(sql As String) As String
+		' hay que leerlo de nuevo
+		Dim Res = New Resultado
+		Dim json As String = ""
+		Dim js As New JavaScriptSerializer()
+		js.MaxJsonLength = 10000000
+
+		dt = New DataTable
+		'Consultar de nuevo para llenar dt
+		Dim Cons2 As New OleDb.OleDbConnection
+		Cons2.ConnectionString = strConexion
+		Cons2.Open()
+		Using dad As New OleDb.OleDbDataAdapter(sql, Cons2)
+
+			Try
+				dad.Fill(dt)
+			Catch ex As Exception
+				MsgBox(ex.Message)
+			End Try
+
+		End Using
+
+		Cons2.Close()
+		Cons2 = Nothing
+
+
+		Res.Status = "OK"
+		Res.errorcode = ""
+		Res.consulta = sql
+		Res.totalRepresentantes = Res.ObtenerRepresentantes()
+		Res.FechaConsulta = Now.ToShortDateString & " " & Now.ToShortTimeString
+		json = js.Serialize(Res)
+
+		If sql = "SELECT * FROM listadoOperaciones where (codrep > 0);" Then
+
+			'Aprovechamos que hacemos una consulta completa y la guardamos
+
+			If File.Exists(HttpContext.Current.Server.MapPath(".") & "\JSON\result.json") Then
+				File.Delete(HttpContext.Current.Server.MapPath(".") & "\JSON\result.json")
+			End If
+			Dim fs As FileStream = File.Create(HttpContext.Current.Server.MapPath(".") & "\JSON\result.json")
+
+			AddText(fs, json)
+
+			fs.Close()
+
+		End If
+
+		dt = Nothing
+
+		Return json
+
+	End Function
+	Private Shared Sub AddText(ByVal fs As FileStream, ByVal value As String)
+		Dim info As Byte() = New UTF8Encoding(True).GetBytes(value)
+		fs.Write(info, 0, info.Length)
+	End Sub
+
+	Private Function CadenaConsulta(parCodRep As String, parCodCli As String, parTipodoc As String, parCodigodoc As String) As String
+		Dim strCadenaConsulta As String
+		Dim strCadenaConsultaBasica As String
+
+		strCadenaConsultaBasica = "SELECT * FROM listadopedidos WHERE ("
+		strCadenaConsulta = strCadenaConsultaBasica
+
+		If (IsNothing(parCodRep) And IsNothing(parCodCli) And IsNothing(parTipodoc) And IsNothing(parCodigodoc)) Then
+
+			strCadenaConsulta = "SELECT * FROM listadopedidos"
+
+		Else
+
+			If Not IsNothing(parCodRep) Then
+				strCadenaConsulta = strCadenaConsulta & "(codrep = " & parCodRep & ")"
+			End If
+
+			If Not IsNothing(parCodCli) Then
+				If Not strCadenaConsulta.Equals(strCadenaConsultaBasica) Then
+					strCadenaConsulta = strCadenaConsulta & " AND "
+				End If
+				strCadenaConsulta = strCadenaConsulta & "(codigo = '" & parCodCli & "')"
+			End If
+
+
+			If Not IsNothing(parTipodoc) Then
+				If Not strCadenaConsulta.Equals(strCadenaConsultaBasica) Then
+					strCadenaConsulta = strCadenaConsulta & " AND "
+				End If
+				strCadenaConsulta = strCadenaConsulta & "(tipodoc = '" & parTipodoc & "')"
+			End If
+
+			If Not IsNothing(parTipodoc) Then
+				If Not strCadenaConsulta.Equals(strCadenaConsultaBasica) Then
+					strCadenaConsulta = strCadenaConsulta & " AND "
+				End If
+				strCadenaConsulta = strCadenaConsulta & "(codigodoc = " & parCodigodoc & ")"
+			End If
+
+			strCadenaConsulta = strCadenaConsulta & ");"
+
+		End If
+
+		Return strCadenaConsulta
+
+	End Function
+
+	Private Function CadenaConsulta2(params As String) As String
+		Dim strCadenaConsulta As String
+		Dim strCadenaConsultaBasica As String
+
+		Dim jss As New JavaScriptSerializer()
+		Dim dict As Dictionary(Of String, String) = jss.Deserialize(Of Dictionary(Of String, String))(params)
+
+
+
+		strCadenaConsultaBasica = "SELECT * FROM listadoOperaciones WHERE ("
+		strCadenaConsulta = strCadenaConsultaBasica
+
+		If (dict("AccesoCli") = "*" And dict("AccesoRep") = "*") Then
+
+			strCadenaConsulta = "SELECT * FROM listadoOperaciones where (codrep > 0);"
+
+		Else
+
+			If dict("AccesoCli") = "*" And dict("AccesoRep") <> "-" Then
+				strCadenaConsulta = strCadenaConsulta & "(codrep IN (" & dict("AccesoRep") & "))"
+			End If
+
+			If dict("AccesoCli") <> "*" And dict("AccesoRep") = "-" Then
+				strCadenaConsulta = strCadenaConsulta & "(codigo = '" & dict("AccesoCli") & "')"
+			End If
+
+
+			strCadenaConsulta = strCadenaConsulta & ");"
+
+		End If
+
+		Return strCadenaConsulta
+
+	End Function
+
+End Class
+
+
+
+
+
+
+
+
+
